bbb01759133f201a5c6adfda1e5b7be7af1ed6c9
Worldspawn <worldspawn.web@gmail.com>
2024-02-06 15:51:40 +0300

    feat: project re-init with VIte

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import TodoList from './components/Counter/Todos/TodoList';
-
-const App: FC = () => {
-  return <TodoList />;
-};
-
-export default App;
--- src/types/todos.ts
+++
@@ -1,6 +0,0 @@
-export type TTodo = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
