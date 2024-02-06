8d28d140a6088e2d184c22befc33d3c703cda55e
Worldspawn <worldspawn.web@gmail.com>
2024-02-06 23:19:34 +0300

    feat: added lazy render

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { routenames } from './routenames';
-import { FC } from 'react';
+import { FC, lazy } from 'react';
 import { Home } from '../pages';
+
+// Lazy Renders
+const RenderPage = lazy(() => import('../pages/React/RenderProps/RenderProps'));
 
 export const Router: FC = () => (
   <Routes>
     <Route path={routenames.HOME} element={<Home />} />
+    <Route path={routenames.RENDER_PROPS} element={<RenderPage />} />
   </Routes>
 );
