ef02701c5dae1bf68df968abc0edf316f093f881
Worldspawn <worldspawn.web@gmail.com>
2024-02-06 19:11:16 +0300

    feat: Home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,23 @@
+import { NavBar } from '../../common';
+import styles from './Home.module.scss';
+
 export const Home = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Welcome to the HomePage!</h1>
+    <div className={styles.page}>
+      <NavBar />
+      <section className={styles.page__greeting}>
+        <img
+          className={styles.page__greeting__img}
+          src="../src/assets/react.svg"
+        />
+        <h1 className={styles.page__greeting__header}>Greetings!</h1>
+        <p className={styles.page__greeting__text}>
+          That's a personal page where I'm learning many aspects of React.js.
+          Usually, all the stuff that is represented here is what I feel not
+          confident about, struggling with applying already existing skills.
+          Hope this project helps me get better at it :)
+        </p>
+      </section>
+    </div>
   );
 };
