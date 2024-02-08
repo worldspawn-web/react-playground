63b4a5eae2c44094984f157462ba75d38cc8484f
Worldspawn <worldspawn.web@gmail.com>
2024-02-08 18:46:29 +0300

    feat: lazy loading for upcoming page Refs

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -5,10 +5,12 @@ import { Home } from '../pages';
 
 // Lazy Renders
 const RenderPage = lazy(() => import('../pages/React/RenderProps/RenderProps'));
+const RefsPage = lazy(() => import('../pages/React/Refs/Refs'));
 
 export const Router: FC = () => (
   <Routes>
     <Route path={routenames.HOME} element={<Home />} />
     <Route path={routenames.RENDER_PROPS} element={<RenderPage />} />
+    <Route path={routenames.REFS} element={<RefsPage />} />
   </Routes>
 );
