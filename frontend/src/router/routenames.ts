f1be0fdbfefe6078e82d0a42c632f040c728555d
Worldspawn <worldspawn.web@gmail.com>
2024-02-13 17:44:51 +0300

    feat: router update

--- frontend/src/router/routenames.ts
+++ frontend/src/router/routenames.ts
@@ -3,5 +3,6 @@ export const routenames = {
   RENDER_PROPS: '/react/render-props',
   REFS: '/react/refs',
   EVENTS: '/react/events',
+  HIGH_ORDER: '/react/high-order-components',
   NOT_FOUND: '*',
 };
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -5,13 +5,17 @@ import { routenames } from './routenames';
 import { Home } from '../pages';
 
 // Lazy Renders
-const RenderPage = lazy(() => import('../pages/React/RenderProps/RenderProps'));
-const RefsPage = lazy(() => import('../pages/React/Refs/Refs'));
+const RenderPage = lazy(() => import('../pages/React/RenderProps'));
+const RefsPage = lazy(() => import('../pages/React/Refs'));
+const EventsPage = lazy(() => import('../pages/React/Events'));
+const HighOrderPage = lazy(() => import('../pages/React/HighOrder'));
 
 export const Router: FC = () => (
   <Routes>
     <Route path={routenames.HOME} element={<Home />} />
     <Route path={routenames.RENDER_PROPS} element={<RenderPage />} />
     <Route path={routenames.REFS} element={<RefsPage />} />
+    <Route path={routenames.EVENTS} element={<EventsPage />} />
+    <Route path={routenames.HIGH_ORDER} element={<HighOrderPage />} />
   </Routes>
 );
